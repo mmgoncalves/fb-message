c6de9cdf6429e8327682c964831ddb40ec3a51e2
Mateus Marques <marques.m05@gmail.com>
2017-12-28 01:11:32 -0200

    first commit

---
+++ FBMessage/FriendsController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  FBMessage
+//
+//  Created by Mateus Marques on 27/12/17.
+//  Copyright © 2017 Mateus Marques. All rights reserved.
+//
+
+import UIKit
+
+class FriendsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    private let cellId = "cellId"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor.white
+        collectionView?.alwaysBounceVertical = true
+        
+        collectionView?.register(FriendCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
+    
+}
+
+class FriendCell: BaseCell {
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override func setupViews() {
+        backgroundColor = UIColor.blue
+        addSubview(profileImageView)
+        
+        addConstraints(<#T##constraints: [NSLayoutConstraint]##[NSLayoutConstraint]#>)
+    }
+}
+
+class BaseCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Init has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = UIColor.blue
+    }
+}
