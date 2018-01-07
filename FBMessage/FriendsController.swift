5925ef5183f9d17d7162ab4fdcfcb474a062045c
Mateus Marques <marques.m05@gmail.com>
2018-01-07 14:07:24 -0200

    create layout

--- FBMessage/FriendsController.swift
+++ FBMessage/FriendsController.swift
@@ -15,6 +15,8 @@ class FriendsController: UICollectionViewController, UICollectionViewDelegateFlo
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.title = "Recent"
+        
         collectionView?.backgroundColor = UIColor.white
         collectionView?.alwaysBounceVertical = true
         
@@ -40,14 +42,100 @@ class FriendCell: BaseCell {
     let profileImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 34
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    let dividerLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0.5, alpha: 0.5)
+        return view
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Mark Zuckerberg"
+        label.font = UIFont.systemFont(ofSize: 18)
+        return label
+    }()
+    
+    let messageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Your friends message and something else..."
+        label.textColor = UIColor.darkGray
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+    
+    let timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "12:05 pm"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textAlignment = .right
+        return label
+    }()
+    
+    let hasReadImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 10
+        imageView.layer.masksToBounds = true
+        
         return imageView
     }()
     
     override func setupViews() {
-        backgroundColor = UIColor.blue
         addSubview(profileImageView)
+        addSubview(dividerLineView)
         
-        addConstraints(<#T##constraints: [NSLayoutConstraint]##[NSLayoutConstraint]#>)
+        setupContainerView()
+        
+        profileImageView.image = UIImage(named: "mark-zuckerberg")
+        hasReadImageView.image = UIImage(named: "mark-zuckerberg")
+        
+        addConstraints(withFormat: "H:|-12-[v0(68)]", views: profileImageView)
+        addConstraints(withFormat: "V:[v0(68)]", views: profileImageView)
+        
+        addConstraints([NSLayoutConstraint(item: profileImageView, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 0)])
+        
+        addConstraints(withFormat: "H:|-82-[v0]|", views: dividerLineView)
+        addConstraints(withFormat: "V:[v0(1)]|", views: dividerLineView)
+    }
+    
+    private func setupContainerView() {
+        let containerView = UIView()
+        addSubview(containerView)
+        
+        addConstraints(withFormat: "H:|-90-[v0]|", views: containerView)
+        addConstraints(withFormat: "V:[v0(50)]", views: containerView)
+        
+        addConstraints([NSLayoutConstraint(item: containerView, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 0)])
+        
+        containerView.addSubview(nameLabel)
+        containerView.addSubview(messageLabel)
+        containerView.addSubview(timeLabel)
+        containerView.addSubview(hasReadImageView)
+        
+        containerView.addConstraints(withFormat: "H:|[v0][v1(80)]-12-|", views: nameLabel, timeLabel)
+        containerView.addConstraints(withFormat: "V:|[v0][v1(24)]|", views: nameLabel, messageLabel)
+        containerView.addConstraints(withFormat: "H:|[v0]-8-[v1(20)]-12-|", views: messageLabel, hasReadImageView)
+        containerView.addConstraints(withFormat: "V:|[v0(24)]", views: timeLabel)
+        containerView.addConstraints(withFormat: "V:[v0(20)]|", views: hasReadImageView)
+    }
+}
+
+extension UIView {
+    func addConstraints(withFormat format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
     }
 }
 
